44de7672b2b410cb3b90ffc244f4801c191b277a
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-06-26 11:19:20 +0300

    Create Address model.

---
+++ src/main/java/bg/codeacademy/cakeShop/model/Address.java
@@ -0,0 +1,21 @@
+package bg.codeacademy.cakeShop.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+    private String city;
+    private String street;
+}
