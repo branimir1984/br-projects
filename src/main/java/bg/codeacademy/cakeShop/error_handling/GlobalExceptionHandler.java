96bac481af310dc91d6d4a660de483707228567d
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-06-27 17:09:50 +0300

    Add GlobalExceptionHandler.

---
+++ src/main/java/bg/codeacademy/cakeShop/error_handling/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package bg.codeacademy.cakeShop.error_handling;
+
+import bg.codeacademy.cakeShop.error_handling.exception.BankAccountExistException;
+import bg.codeacademy.cakeShop.error_handling.exception.UniqueIdentificationNumberExistException;
+import bg.codeacademy.cakeShop.error_handling.exception.UserNameExistException;
+import bg.codeacademy.cakeShop.error_handling.exception.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({BankAccountExistException.class})
+    public ResponseEntity<Object> handleBankAccountExistException(BankAccountExistException exception) {
+        exception.printStackTrace(System.out);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+    @ExceptionHandler({UniqueIdentificationNumberExistException.class})
+    public ResponseEntity<Object> handleUniqueIdentificationNumberExistException(UniqueIdentificationNumberExistException exception) {
+        exception.printStackTrace(System.out);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+    @ExceptionHandler({UserNameExistException.class})
+    public ResponseEntity<Object> handUserNameExistException(UserNameExistException exception) {
+        exception.printStackTrace(System.out);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+    @ExceptionHandler({UserNotFoundException.class})
+    public ResponseEntity<Object> handUserNotFoundException(UserNotFoundException exception) {
+        exception.printStackTrace(System.out);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        LinkedHashMap<String, Object> errorsMap = new LinkedHashMap<>();
+        for (ObjectError err : errors) {
+            errorsMap.put(((FieldError) err).getField(), err.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
+    }
+}
