499102012afba481fc3cc620f66bebf865083210
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-06-28 00:40:06 +0300

    Create mapToPersonalData method in Mapper class.

--- src/main/java/bg/codeacademy/cakeShop/mapper/Mapper.java
+++ src/main/java/bg/codeacademy/cakeShop/mapper/Mapper.java
@@ -2,6 +2,7 @@ package bg.codeacademy.cakeShop.mapper;
 
 import bg.codeacademy.cakeShop.dto.BankAccountDTO;
 import bg.codeacademy.cakeShop.dto.LegalEntityRegistrationDTO;
+import bg.codeacademy.cakeShop.dto.PersonalDataDTO;
 import bg.codeacademy.cakeShop.enums.Currency;
 import bg.codeacademy.cakeShop.enums.Role;
 import bg.codeacademy.cakeShop.model.Address;
@@ -16,16 +17,7 @@ import java.util.List;
 @Component
 public class Mapper {
     public LegalEntity mapToLegalEntity(LegalEntityRegistrationDTO dto) {
-        Address address = new Address();
-        address.setCity(dto.personalData().address().city());
-        address.setStreet(dto.personalData().address().street());
-
-        PersonalData personalData = new PersonalData();
-        personalData.setUserName(dto.personalData().userName());
-        personalData.setUserPassword(dto.personalData().password());
-        personalData.setUserRole(Role.valueOf(dto.personalData().role()));
-        personalData.setAddress(address);
-        personalData.setPersonalName(dto.personalData().personalName());
+        PersonalData personalData = mapToPersonalData(dto.personalData());
 
         List<BankAccountDTO> dtoList = dto.personalData().bankAccount();
         List<BankAccount> accounts = new LinkedList<>();
@@ -45,5 +37,18 @@ public class Mapper {
         legalEntity.setEmail(dto.email());
         legalEntity.setPersonalData(personalData);
         return legalEntity;
+    }
+
+    public PersonalData mapToPersonalData(PersonalDataDTO dto) {
+        Address address = new Address();
+        address.setCity(dto.address().city());
+        address.setStreet(dto.address().street());
+        PersonalData personalData = new PersonalData();
+        personalData.setUserName(dto.userName());
+        personalData.setUserPassword(dto.password());
+        personalData.setUserRole(Role.valueOf(dto.role()));
+        personalData.setAddress(address);
+        personalData.setPersonalName(dto.personalName());
+        return personalData;
     }
 }
