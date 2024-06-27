186f1530ebdcd76faf18c9775565fd7936dd5377
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-06-28 00:30:30 +0300

    Add registerStaff method in RegistrationService class.

--- src/main/java/bg/codeacademy/cakeShop/service/RegistrationService.java
+++ src/main/java/bg/codeacademy/cakeShop/service/RegistrationService.java
@@ -2,6 +2,9 @@ package bg.codeacademy.cakeShop.service;
 
 import bg.codeacademy.cakeShop.model.Address;
 import bg.codeacademy.cakeShop.model.LegalEntity;
+import bg.codeacademy.cakeShop.model.PersonalData;
+import bg.codeacademy.cakeShop.model.Staff;
+import bg.codeacademy.cakeShop.repository.StaffRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +14,18 @@ public class RegistrationService {
     private final BankAccountService bankAccountService;
     private final PersonalDataService personalDataService;
     private final LegalEntityService legalEntityService;
+    private final StaffRepository staffRepository;
 
     public RegistrationService(AddressService addressService,
                                BankAccountService bankAccountService,
                                PersonalDataService personalDataService,
-                               LegalEntityService legalEntityService) {
+                               LegalEntityService legalEntityService,
+                               StaffRepository staffRepository) {
         this.addressService = addressService;
         this.bankAccountService = bankAccountService;
         this.personalDataService = personalDataService;
         this.legalEntityService = legalEntityService;
+        this.staffRepository = staffRepository;
     }
 
     @Transactional
@@ -30,5 +36,22 @@ public class RegistrationService {
         bankAccountService.addBankAccount(legalEntity.getPersonalData().getBankAccount());
         legalEntityService.addLegalEntity(legalEntity);
         return legalEntity.getUin();
+    }
+
+    @Transactional
+    public String registerStaff(PersonalData personalData, String principal) {
+        Address address = addressService.addAddress(personalData.getAddress());
+        personalData.setAddress(address);
+        personalDataService.addPersonalData(personalData);
+        bankAccountService.addBankAccount(personalData.getBankAccount());
+        Staff staff = new Staff();
+
+        PersonalData principalPersonalData = personalDataService.getByUserName(principal);
+        LegalEntity legalEntity = legalEntityService.getLegalEntityById(principalPersonalData.getId());
+
+        staff.setEmployer(legalEntity);
+        staff.setPersonalData(personalData);
+        staffRepository.save(staff);
+        return personalData.getUserName();
     }
 }
