15584f8b9ab688969a807cf39b9fe8fc5684b0fe
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-06-28 16:29:11 +0300

    Create OfferRepository.

---
+++ src/main/java/bg/codeacademy/cakeShop/repository/OfferRepository.java
@@ -0,0 +1,12 @@
+package bg.codeacademy.cakeShop.repository;
+
+import bg.codeacademy.cakeShop.model.Address;
+import bg.codeacademy.cakeShop.model.LegalEntity;
+import bg.codeacademy.cakeShop.model.Offer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OfferRepository extends CrudRepository<Offer, Integer> {
+    boolean existsOfferByOfferorAndMoney(LegalEntity offeror, float money);
+
+    Offer findOfferByOfferorAndMoney(LegalEntity offeror, float money);
+}
