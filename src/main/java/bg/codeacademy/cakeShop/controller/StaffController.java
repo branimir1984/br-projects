f6a922ef5d9a096533aad4b0d99df23f8f641e07
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-07-03 13:54:14 +0300

    Start of using the ID of authenticated user instead of the name.

--- src/main/java/bg/codeacademy/cakeShop/controller/StaffController.java
+++ src/main/java/bg/codeacademy/cakeShop/controller/StaffController.java
@@ -1,24 +1,23 @@
 package bg.codeacademy.cakeShop.controller;
 
-import bg.codeacademy.cakeShop.dto.LegalEntityRegistrationDTO;
 import bg.codeacademy.cakeShop.dto.PersonalDataDTO;
 import bg.codeacademy.cakeShop.mapper.Mapper;
-import bg.codeacademy.cakeShop.model.LegalEntity;
 import bg.codeacademy.cakeShop.model.PersonalData;
 
+import bg.codeacademy.cakeShop.security.AuthenticatedUser;
 import bg.codeacademy.cakeShop.service.StaffService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/ver1/staff")
+@RequestMapping("/api/v1/staff")
 public class StaffController {
     private final StaffService staffService;
     private final Mapper mapper;
@@ -28,16 +27,14 @@ public class StaffController {
         this.mapper = mapper;
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, path = "/save")
-    public ResponseEntity<String> save(@RequestBody PersonalDataDTO dto) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createStaff(
+            Authentication authentication, @Valid @RequestBody PersonalDataDTO dto) {
+        AuthenticatedUser user = (AuthenticatedUser) authentication;
         PersonalData personalData = mapper.mapToPersonalData(dto);
-        return new ResponseEntity<>(staffService.addStaff(
-                personalData, getPrincipalName()) +
-                " successfully registered as " + dto.role() + " with employer:" + getPrincipalName(), HttpStatus.CREATED);
-    }
-
-    public String getPrincipalName() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication.getName();
+        return new ResponseEntity<>(staffService.createStaff(
+                personalData, user.getId()) +
+                " successfully registered as " + dto.role() + " with employer:"
+                + authentication.getName(), HttpStatus.CREATED);
     }
 }
